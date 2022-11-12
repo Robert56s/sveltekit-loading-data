a424a3aac97022a78f261f822051ba3b70aecef1
Robert56s <78372422+Robert56s@users.noreply.github.com>
2022-11-12 18:29:37 -0500

    idk this shit

--- src/routes/shop/+page.js
+++ src/routes/shop/+page.js
@@ -4,6 +4,7 @@ export const load = async ({ fetch }) => {
     const fetchProducts = async () => {   
         const productRes = await fetch('https://dummyjson.com/products?limit=10')
         const productData = await productRes.json()
+        console.log(productData.products)
         return productData.products
     }
 
